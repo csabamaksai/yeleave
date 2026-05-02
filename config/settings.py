8003eb5163f08950da0779b5576953e5452f7e94
Csaba Maksai <csmaksai@gmail.com>
2026-05-02 08:53:16 +0200

    feat: setup custom user model

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Saját appok
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
